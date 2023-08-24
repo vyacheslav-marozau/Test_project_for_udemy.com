3ae89f4dd58f1429f6b9d77e45de67516347b667
Vyachaslau Marozau <simonfenix@tut.by>
2023-08-24 23:26:17 +0300

    29. Praktika, ch. 3 Ispolsuem funkcii

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '8');
+'use strict';
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '8');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '8');
+    }
+}
+
+start();
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -6,13 +18,86 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+/*let w = 1;
+while (w <= 2) {
+    let key = prompt('Один из последних просмотренных фильмов?', ''),
+        sense = prompt('Как вы его оцените?', '10');
+    if (key != null && sense != null && key != ' ' && sense != ' ' && key.length < 50 ) {
+        personalMovieDB.movies[key] = sense;
+        console.log('done');
+        console.log(personalMovieDB);
+    } else {
+        alert ('Вы не ввели название фильма (либо ввели название фильма более 50 символов) и/или его оценку, ведите название фильма и оценку');
+        console.log('error');
+        w--;
+    }
+    w++;
+}*/   // цикл while
+/*let w = 1;
+do {
+    let key = prompt('Один из последних просмотренных фильмов?', ''),
+        sense = prompt('Как вы его оцените?', '10');
+    if (key != null && sense != null && key != ' ' && sense != ' ' && key.length < 50 ) {
+        personalMovieDB.movies[key] = sense;
+        console.log('done');
+        console.log(personalMovieDB);
+    } else {
+        alert ('Вы не ввели название фильма (либо ввели название фильма более 50 символов) и/или его оценку, ведите название фильма и оценку');
+        console.log('error');
+        w--;
+    }
+    w++;
+} while (w <= 2)*/   // цикл do while
 
-const key1 = prompt('Один из последних просмотренных фильмов?', ''),
-      sense1 = prompt('Как вы его оцените?', '10'),
-      key2 = prompt('Один из последних просмотренных фильмов?', ''),
-      sense2 = prompt('Как вы его оцените?', '10');
-personalMovieDB.movies[key1] = sense1;
-personalMovieDB.movies[key2] = sense2;
+function rememberMyFilms() {
+    for (let w = 1; w <= 2; w++) {
+        let key = prompt('Один из последних просмотренных фильмов?', ''),
+            sense = prompt('Как вы его оцените?', '10');
+        if (key != null && sense != null && key != ' ' && sense != ' ' && key.length < 50 ) {
+            personalMovieDB.movies[key] = sense;
+            console.log('done');
+            //console.log(personalMovieDB);
+        } else {
+            alert ('Вы не ввели название фильма (либо ввели название фильма более 50 символов) и/или его оценку, ведите название фильма и оценку');
+            console.log('error');
+            w--;
+        }
+    }   // цикл for
+}
 
-console.log(personalMovieDB);
+rememberMyFilms();
 
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert('Просмотрено довольно мало фильмов!');
+    } else if ( 10 <= personalMovieDB.count <= 30 ) {
+        alert('Вы классичесский зритель!');
+    } else if ( personalMovieDB.count > 30) {
+        alert('Вы киноман!');
+    } else {
+        alert('Произошла Ошибка');
+    }
+}
+
+detectPersonalLevel();
+
+// 2/ Создать функцию showMyDB, которая будет проверять свойство privat.
+// Если стоит в позиции false - выводит в консоль главный объект программы
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+// 3/ Создать функцию writeYourGenres в которой пользователь будет 3 раза
+// отвечать на вопрос "Ваш любимый жанр под номером ${номер по порядку}".
+// Каждый ответ записывается в массив данных genres
+
+function writeYourGenres() {
+    for (let w = 0; w <= 2; w++) {
+        personalMovieDB.genres[w] = prompt(`Ваш любимый жанр под номером ${w+1}`)
+    }
+}
+writeYourGenres();
